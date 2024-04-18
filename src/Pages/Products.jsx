e22fbbd76a16ffc856db4c3f4275b01870e4056f
atharv <arm3scien@gmail.com>
2024-04-18 15:00:45 +0530

    added arrows in products component

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,19 +1,53 @@
+// Products.js
 import React, { useEffect, useState } from "react";
 import MedicineList from "../Products/MedicineList";
 import Title from "../components/Title";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Products = ({ selectedProduct }) => {
+  const [currentProductIndex, setCurrentProductIndex] = useState(0);
+  const allProducts = ["Product A", "Product B" /* Add more products here */];
+
   useEffect(() => {
-    document.title = `Products - ${selectedProduct || "Merimedian"}`;
+    document.title = `Products - ${selectedProduct || "Merimeridian"}`;
   }, [selectedProduct]);
+
+  const updateTitle = (index) => {
+    document.title = `Products - ${allProducts[index] || "Merimeridian"}`;
+  };
+
+  const goToNextProduct = () => {
+    const nextIndex = (currentProductIndex + 1) % allProducts.length;
+    setCurrentProductIndex(nextIndex);
+    updateTitle(nextIndex);
+  };
+
+  const goToPreviousProduct = () => {
+    const previousIndex =
+      (currentProductIndex - 1 + allProducts.length) % allProducts.length;
+    setCurrentProductIndex(previousIndex);
+    updateTitle(previousIndex);
+  };
 
   return (
     <div>
-      {selectedProduct && <Title text={selectedProduct} />}
+      {/* <Title text={selectedProduct} updateTitle={updateTitle} /> */}
 
-      {selectedProduct && <MedicineList selectedProduct={selectedProduct} />}
+      {selectedProduct && (
+        <div className="d-flex justify-content-between align-items-center mt-3 px-3">
+          <div className="icon-container" onClick={goToPreviousProduct}>
+            <FontAwesomeIcon icon={faArrowLeft} size="2x" />
+          </div>
+          <div className="icon-container" onClick={goToNextProduct}>
+            <FontAwesomeIcon icon={faArrowRight} size="2x" />
+          </div>
+        </div>
+      )}
+
+      {selectedProduct && (
+        <MedicineList selectedProduct={allProducts[currentProductIndex]} />
+      )}
     </div>
   );
 };

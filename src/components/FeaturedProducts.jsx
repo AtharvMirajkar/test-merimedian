e128c2bc24104751fd581af43cadd0e337f660f4
atharv <arm3scien@gmail.com>
2024-04-21 12:45:14 +0530

    added some more featured product

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,17 +1,33 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import product1 from "../assets/medicine1.jpg";
-import product2 from "../assets/medicine2.png";
-import product3 from "../assets/medicine3.png";
+import product1 from "../assets/Products/product1.png";
+import product2 from "../assets/Products/product2.png";
+import product3 from "../assets/Products/product3.png";
+import product4 from "../assets/Products/product4.png";
+import product5 from "../assets/Products/product5.png";
+import product6 from "../assets/Products/product6.png";
+import product7 from "../assets/Products/product7.png";
+import product8 from "../assets/Products/product8.png";
 
 const FeaturedProducts = () => {
+  const navigate = useNavigate();
   const products = [
-    { id: 1, title: "Flatuma", imageUrl: product1 },
-    { id: 2, title: "MZORB D3", imageUrl: product2 },
-    { id: 3, title: "Cis Q Fortee", imageUrl: product3 },
+    { id: "MAZORB-D3", title: "MAZORB-D3", imageUrl: product1 },
+    { id: "FB-Rich", title: "FB Rich", imageUrl: product2 },
+    { id: "CIS-Q-Forte", title: "Cis Q Fortee", imageUrl: product3 },
+    { id: "UPCOOL-20", title: "UPCOOL 20", imageUrl: product4 },
+    { id: "UPTRACE", title: "UPTRACE", imageUrl: product5 },
+    { id: "Ne-norm", title: "Ne-norm", imageUrl: product6 },
+    { id: "Pilenorm", title: "Pilenorm", imageUrl: product7 },
+    {
+      id: "UPTACE-M Injection",
+      title: "UPTACE-M Injection",
+      imageUrl: product8,
+    },
     // Add more products as needed
   ];
 
@@ -38,6 +54,11 @@ const FeaturedProducts = () => {
     ],
   };
 
+  const handleProductClick = (productId) => {
+    // Navigate to the products page and pass the selected product ID as a URL parameter
+    navigate(`/products/${productId}`);
+  };
+
   return (
     <div className="container mt-5 mb-5">
       <h2 className="text-center">
@@ -53,8 +74,18 @@ const FeaturedProducts = () => {
                   className="img-fluid rounded mx-auto" // Center image horizontally
                   alt={product.title}
                   style={{ maxHeight: "200px" }} // Specify dimensions here
+                  onClick={() => handleProductClick(product.id)} // Handle click event
+                  role="button" // Add role button for accessibility
+                  tabIndex="0" // Add tabIndex for accessibility
                 />
-                <h4 style={{ fontSize: "1.rem" }}>{product.title}</h4>
+                <h4
+                  style={{ fontSize: "1.1rem" }}
+                  onClick={() => handleProductClick(product.id)} // Handle click event
+                  role="button" // Add role button for accessibility
+                  tabIndex="0" // Add tabIndex for accessibility
+                >
+                  {product.title}
+                </h4>
               </div>
             ))}
           </Slider>
